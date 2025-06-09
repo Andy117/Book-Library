7aa44919f9b6e3268e5b08c74bc1a8cd7cf3a96f
Anderson Velasquez <andersonjosvela@gmail.com>
2025-06-09 10:20:07 -0600

    Use of ReactRouter and creation of the route '/' in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import Books from "./components/Books";
 import { BooksProvider } from "./context/BookContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BooksProvider>
-      <></>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Books></Books>}></Route>
+        </Routes>
+      </Router>
     </BooksProvider>
   );
 }
---
+++ src/components/Books.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { BookContext } from "../context/BookContext.jsx";
+
+function Books() {
+  const books = useContext(BookContext);
+
+  return (
+    <div>
+      <h1>All Books Available</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title} by {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Books;
